9f4e2234bc4ded605cc226dc39f53bc4a5597f06
aryansyah1048 <mahendraaryansyah98@gmail.com>
2025-02-25 22:07:13 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ let thumbnails = document.querySelectorAll(".thumbnail .item");
 let countItem = items.length;
 let itemActive = 0;
 // event next click
-next.onclick = function () {
+next.onclick = function (event) {
+  event.preventDefault();
   itemActive = itemActive + 1;
   if (itemActive >= countItem) {
     itemActive = 0;
@@ -16,7 +17,8 @@ next.onclick = function () {
   showSlider();
 };
 //event prev click
-prev.onclick = function () {
+prev.onclick = function (event) {
+  event.preventDefault();
   itemActive = itemActive - 1;
   if (itemActive < 0) {
     itemActive = countItem - 1;
@@ -26,7 +28,7 @@ prev.onclick = function () {
 // auto run slider
 let refreshInterval = setInterval(() => {
   next.click();
-}, 5000);
+}, 1000000);
 function showSlider() {
   // remove item active old
   let itemActiveOld = document.querySelector(".slider .list .item.active");
@@ -59,4 +61,25 @@ thumbnails.forEach((thumbnail, index) => {
     itemActive = index;
     showSlider();
   });
+});
+
+// hamburger menu
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden"); // Sembunyikan/munculkan menu
+  mobileMenu.classList.toggle("block"); // Pastikan muncul saat di-toggle
+});
+
+// navbar
+window.addEventListener("scroll", function () {
+  var navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled", "py-0");
+    navbar.classList.remove("bg-transparent");
+  } else {
+    navbar.classList.remove("scrolled", "py-0");
+    navbar.classList.add("bg-transparent");
+  }
 });
